a66a610991f4f6820388ee2780335627c0457fb3
Matt <68249412+MattGodwin@users.noreply.github.com>
2021-02-02 19:31:34 +0000

    auto roles

--- cogs/greetings.py
+++ cogs/greetings.py
@@ -9,12 +9,24 @@ class Greetings(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
+
+        #Auto Role On Join
+        await member.add_roles(discord.utils.get(member.guild.roles, name='Members'))
+        print(f"{member} was given the {discord.utils.get(member.guild.roles, name='Members')} role.")
+
+        #Welcome Message
+
         channel = member.guild.system_channel
         if channel is not None:
-            await channel.send('Welcome {0.mention}.'.format(member))
+            embed = discord.Embed(title='Welcome @{0}.'.format(member), description="Hope that you have a good time!",
+                                  color=0xFFB6C1)
+            await channel.send(embed=embed)
 
     @commands.command()
     async def hello(self, ctx, *, member: discord.Member = None):
+
+        #Hello Command
+
         member = member or ctx.author
         if self._last_member is None or self._last_member.id != member.id:
             await ctx.send('Hello {0.name}~'.format(member))
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 
-bot = commands.Bot(command_prefix='.', help_command=None)
+bot = commands.Bot(command_prefix='.', help_command=None, intents=discord.Intents.all())
 client = discord.Client()
 initial_extensions = ['cogs.greetings', 'cogs.utility', 'cogs.games']
 
